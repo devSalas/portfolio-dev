34ad33f460652b2f73cbf625aa47162068d13eb8
Esteban salas sulca <esalassulca@gmail.com>
2023-09-13 15:45:25 -0500

    agregar compoentes contactos,header,about,proyectos

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+			  'backCode': "url('./code-light.jpg')",
+			},
+			gridTemplateColumns:{
+				'grilla':"repeat(auto-fit,minmax(200px,1fr))",
+			},
+			
+			
+		},
+	plugins: [],
+	}
+}
